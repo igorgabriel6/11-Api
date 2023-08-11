96f13722cba4516a11fb0edd8869be091941e1c1
igorgabriel6 <132698279+igorgabriel6@users.noreply.github.com>
2023-08-11 12:45:01 +0000

    update

--- bd.js
+++ bd.js
@@ -16,3 +16,15 @@ async function selectUsuarios() {
 }
 
 export { selectUsuarios };
+
+
+async function selectUsuario(id) {
+  const client = await connect();
+  const query = "SELECT * FROM usuario WHERE id = $1";
+  const usuario = [id];
+  const res = await client.query(query, usuario);
+  return res.rows;
+}
+
+
+export { selectUsuarios, selectUsuario };
--- index.js
+++ index.js
@@ -29,3 +29,18 @@ app.get("/usuarios", async (req, res) => {
     res.status(error.status || 500).json({ message: error.message || "Erro!" });
   }
 });
+
+
+import { selectUsuarios, selectUsuario } from "./bd.js";
+
+
+app.get("/usuario/:id", async (req, res) => {
+  console.log("Rota GET /usuario solicitada");
+  try {
+    const usuario = await selectUsuario(req.params.id);
+    if (usuario.length > 0) res.json(usuario);
+    else res.status(404).json({ message: "Usuário não encontrado!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
