7c1ba3d86119138ec0ce4649a84439eb0b6fd0e6
igorgabriel6 <132698279+igorgabriel6@users.noreply.github.com>
2023-08-25 17:00:35 +0000

    update

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import express from "express";      // Requisição do pacote do express
+import dotenv from "dotenv";
+dotenv.config();
+
+
+
+const app = express();              // Instancia o Express
+const port = 3000;                  // Define a porta
+app.use(express.json());
+
+app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
+  res.json({
+    nome: "Ígor Gabriel Sena Matos",      // Substitua pelo seu nome
+  });
+  console.log("Rota / solicitada");
+});
+
+app.listen(port, () => {            // Um socket para "escutar" as requisições
+  console.log(`Serviço escutando na porta:  ${port}`);
+});
+
+//index.js
+
+
+
