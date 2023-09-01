7673c4f0bd6d18bf8dbb4cd0daed0c719ba4e1c9
igorgabriel6 <132698279+igorgabriel6@users.noreply.github.com>
2023-09-01 12:59:07 +0000

    update

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ app.use(loginRouter);
 
 app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
   res.json({
-    nome: "Gabriela de Souza Reis",      // Substitua pelo seu nome
+    nome: "Ígor Gabriel Sena Matos",      // Substitua pelo seu nome
   });
   console.log("Rota / solicitada");
 });
